8a51ea85455bc249726a3f8182e6b3c4f4e47540
bluebear94 <uruwi@protonmail.com>
2017-07-31 03:49:55 -0400

    Fix codez

--- src/libs/DraHyl_rase/stress.py
+++ src/libs/DraHyl_rase/stress.py
@@ -49,46 +49,68 @@ class Nucleus:
 class Short(Nucleus):
   def __init__(self, vowel: int):
     self.vowel = vowel
-  def typ() -> int:
+  def typ(self) -> int:
     return SHORT
-  def decode(mode: int) -> Optional[str]:
-    if mode == NO_ACCENT: return vowel_constant_to_short(vowel)
-    if mode == VARIANT_ACCENT: return vowel_constant_to_short_stress(vowel)
+  def decode(self, mode: int) -> Optional[str]:
+    if mode == NO_ACCENT:
+      return vowel_constant_to_short(self.vowel)
+    if mode == VARIANT_ACCENT:
+      return vowel_constant_to_short_stress(self.vowel)
+  def raised(self) -> Nucleus:
+    return Short(self.vowel + (self.vowel % 3 != 2))
 
 class Long(Nucleus):
   def __init__(self, vowel: int):
     self.vowel = vowel
-  def typ() -> int:
+  def typ(self) -> int:
     return LONG
-  def decode(mode: int) -> Optional[str]:
-    if mode == NO_ACCENT: return vowel_constant_to_long(vowel)
-    if mode == VARIANT_ACCENT_2: return vowel_constant_to_long_lax(vowel)
+  def decode(self, mode: int) -> Optional[str]:
+    if mode == NO_ACCENT:
+      return vowel_constant_to_long(self.vowel)
+    if mode == VARIANT_ACCENT_2:
+      return vowel_constant_to_long_lax(self.vowel)
+  def raised(self) -> Nucleus:
+    return Long(self.vowel + (self.vowel % 3 != 2))
 
 class DiphthongLG(Nucleus):
   def __init__(self, glide: int, vowel: int):
     self.glide = glide
     self.vowel = vowel
-  def typ() -> int:
+  def typ(self) -> int:
     return DIPHTHONG_LG
-  def decode(mode: int) -> Optional[str]:
+  def decode(self, mode: int) -> Optional[str]:
     n = ""
-    if mode == NO_ACCENT: n = vowel_constant_to_short(vowel)
-    if mode == VARIANT_ACCENT: n = vowel_constant_to_short_stress(vowel)
-    if mode == VARIANT_ACCENT_2: n = vowel_constant_to_short_lax(vowel)
+    if mode == NO_ACCENT:
+      n = vowel_constant_to_short(self.vowel)
+    if mode == VARIANT_ACCENT:
+      n = vowel_constant_to_short_stress(self.vowel)
+    if mode == VARIANT_ACCENT_2:
+      n = vowel_constant_to_short_lax(self.vowel)
     return vowel_constant_to_glide(glide) + n
+  def raised(self) -> Nucleus:
+    newVowel = self.vowel + (self.vowel % 3 != 2)
+    if newVowel == self.glide: return Long(newVowel)
+    return DiphthongLG(self.glide, newVowel)
 
 class DiphthongRG(Nucleus):
   def __init__(self, vowel: int, glide: int):
     self.vowel = vowel
     self.glide = glide
-  def typ() -> int:
+  def typ(self) -> int:
     return DIPHTHONG_RG
-  def decode(mode: int) -> Optional[str]:
+  def decode(self, mode: int) -> Optional[str]:
     n = ""
-    if mode == NO_ACCENT: n = vowel_constant_to_short(vowel)
-    if mode == VARIANT_ACCENT: n = vowel_constant_to_short_stress(vowel)
-    if mode == VARIANT_ACCENT_2: n = vowel_constant_to_short_lax(vowel)
+    if mode == NO_ACCENT:
+      n = vowel_constant_to_short(self.vowel)
+    if mode == VARIANT_ACCENT:
+      n = vowel_constant_to_short_stress(self.vowel)
+    if mode == VARIANT_ACCENT_2:
+      n = vowel_constant_to_short_lax(self.vowel)
     return n + vowel_constant_to_glide(glide)
+  def raised(self) -> Nucleus:
+    newVowel = self.vowel + (self.vowel % 3 != 2)
+    if newVowel == self.glide: return Long(newVowel)
+    return DiphthongRG(newVowel, self.glide)
 
 # def string_to_nucleus
 
@@ -107,16 +129,20 @@ def is_valid_coda_to_short(coda: str) -> bool:
   return coda in valid_short_codas
 
 class Syllable:
-  def __init__(self, onset: str, nucleus: Nucleus, coda: str):
+  def __init__(self, onset: str, nucleus: Nucleus, coda: str = ""):
     # A syllable cannot have both a diphthong nucleus and a coda
-    if nucleus.typ() >= DIPHTHONG_LM and coda != "":
+    if nucleus.typ() >= DIPHTHONG_LG and coda != "":
       raise MalformedSyllableException("Can't have both diphthong nucleus and coda!")
-    if nucleus.typ() == SHORT and not is_valid_coda_to_short(coda)
+    if nucleus.typ() == SHORT and not is_valid_coda_to_short(coda) and coda != "":
       raise MalformedSyllableException(coda + " is not a valid coda to a short vowel!")
     self.onset = onset
     self.nucleus = nucleus
     self.coda = coda
-  def decode(mode: int) -> Optional[str]:
-    nstr = nucleus.decode()
+  def decode(self, mode: int) -> Optional[str]:
+    nstr = self.nucleus.decode(mode)
     if nstr is None: return None
-    return onset + nstr + coda
+    return self.onset + nstr + self.coda
+
+exsyll = Syllable("b", Short(I), "m")
+exsyll2 = Syllable("b", Long(O))
+print(exsyll.decode(NO_ACCENT) + exsyll2.decode(VARIANT_ACCENT_2))
